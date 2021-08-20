ee05a8ca3868dd2ee7008f4dab08e4b4f465bf73
TAMIM <saifur.tamim@gmail.com>
2021-08-21 00:53:42 +0600

    full complete

---
+++ script.js
@@ -0,0 +1,42 @@
+
+const subTotalField = document.getElementById('subTotal-price');
+
+document.querySelectorAll('.options').forEach(function (element) {
+    element.addEventListener('click', function (event) {
+        if (event.target.localName == 'button') {
+            this.querySelector('.selected').classList.remove('selected');
+            event.target.classList.add('selected');
+
+            updateCost(this.id, event.target.id);
+        }
+    })
+})
+function updateCost(product, extraCost) {
+    document.getElementById('extra-' + product + '-cost').innerText = extraCost;
+    updateSubTotal();
+}
+
+function updateSubTotal() {
+    const basePrice = 1299;
+
+    let subTotal = basePrice;
+    const extraPrices = document.querySelectorAll('.extra-cost');
+    for (const value of extraPrices) {
+        subTotal += parseFloat(value.innerText);
+    }
+    subTotalField.innerText = subTotal;
+    updateTotal();
+}
+
+function updateTotal() {
+    const promoField = document.getElementById('promo-input');
+    const subTotalAmount = parseFloat(subTotalField.innerText);
+    let total;
+
+    if (promoField.value == 'stevekaku') {
+        const discount = subTotalAmount * 0.2;
+        total = subTotalAmount - discount;
+    } else { total = subTotalAmount }
+
+    document.getElementById('total-amount').innerText = total;
+}
